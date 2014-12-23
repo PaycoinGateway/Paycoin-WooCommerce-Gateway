4520a2b5b3f9ae22f794c94d293645c933b5f8f2
master0yoshigr <simdude@gmail.com>
2014-12-23 08:26:38 -0500

    Base Wordpress Plugin Setup

---
+++ Paycoin-Gateway.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Paycoin WooCommerce Gateway
+Plugin URI: https://github.com/master0yoshigr/Paycoin-WooCommerce-Gateway/
+Description: This Wordpress plugin will add a paycoin gateway to WooCommerce
+Version: 1.0
+Author: master0yoshigr
+Author URI: https://github.com/master0yoshigr/
+License: GPL2
+*/
